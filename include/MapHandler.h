39702b004e9ed40806c960724f0f444be3a94c5c
matteoquesada <matteo.vargas.quesada@est.una.ac.cr>
2023-11-21 19:50:53 -0600

    Complete save/load support without bugcheck

--- include/MapHandler.h
+++ include/MapHandler.h
@@ -114,26 +114,96 @@ class MapHandler
 
 		void save()
 		{
-			allRoutes.save();
+			ofstream file("userdata/routes.txt");
+			if (!file.is_open()) {
+				cerr << "ERROR GARRAFAL: SE QUEMARON LOS FRIJOLES." << endl;
+				return;
+			}
+
+			Node<Route>* temp = allRoutes.head; // MAKE A TEMPORARY POINTER TO THE HEAD OF THE LIST
+			Node<Coordinates>* subTemp = nullptr; // MAKE A TEMPORARY POINTER TO THE HEAD OF THE SUB-LIST
+
+			while (temp != nullptr) { // ITERATE THROUGH THE ROUTES
+
+				file << "<Route>" << endl;
+				file << "Color: ";
+				file << static_cast<int>(temp->data.color.r) << " " // SAVE THE R CHANNEL INTO THE FILE
+					<< static_cast<int>(temp->data.color.g) << " " // SAVE THE G CHANNEL INTO THE FILE
+					<< static_cast<int>(temp->data.color.b) << std::endl; // SAVE THE B CHANNEL INTO THE FILE
+
+				subTemp = temp->data.head; // MAKE A TEMPORARY POINTER TO THE HEAD OF THE SUB-LIST
+
+				while (subTemp != nullptr) { // ITERATE THROUGH THE SUB-LIST
+					file << subTemp->data.getX() << " " << subTemp->data.getY() << endl; // SAVE THE COORDINATES INTO THE FILE
+					subTemp = subTemp->next; // MOVE TO THE NEXT NODE
+				}
+				file << "</>" << endl; // END OF THE ROUTE
+				temp = temp->next; // MOVE TO THE NEXT ROUTE
+			}
 		}
 
 		void load()
 		{
 			Node<Route>* ptrToHead = allRoutes.head;
-			allRoutes.load(ptrToHead);
+			ifstream file("userdata/routes.txt");
+			if (!file.is_open()) {
+				cerr << "ERROR GARRAFAL: SE QUEMARON LOS FRIJOLES." << endl;
+				return;
+			}
+
+			string xxx;
+			while (getline(file, xxx)) {
+				cout << xxx << endl;
+				if (xxx == "<Route>") {
+					// Start a new route
+					Route route;
+					getline(file, xxx); // Skip the "<Route>" line
+
+					int r, g, b;
+					xxx.erase(0, 7); // Remove the "Color: " part
+
+					r = stoi(xxx.substr(0, xxx.find(" "))); // Get the R channel
+					xxx.erase(0, xxx.find(" ") + 1); // Remove the R channel
+					g = stoi(xxx.substr(0, xxx.find(" "))); // Get the G channel
+					xxx.erase(0, xxx.find(" ") + 1); // Remove the G channel
+					b = stoi(xxx.substr(0, xxx.find(" "))); // Get the B channel
+					xxx.erase(0, xxx.find(" ") + 1); // Remove the B channel
+					cout << "Found R: " << r << " Found G: " << g << " Found B: " << b << endl;
+
+					Color color(r, g, b); // CREATE THE COLOR BASED ON THE VALUES WE JUST GOT
+					route.color = color; // ASSIGN THE COLOR TO THE ROUTE
+
+					// Now we need to get the coordinates
+					while (getline(file, xxx)) {
+						if (xxx == "</>") {
+							// End of the route
+							break;
+						}
+						else {
+							// Add the coordinates to the route
+							int x, y;
+							x = stoi(xxx.substr(0, xxx.find(" "))); // Get the X coordinate
+							xxx.erase(0, xxx.find(" ") + 1); // Remove the X coordinate
+							y = stoi(xxx.substr(0, xxx.find(" "))); // Get the Y coordinate
+							xxx.erase(0, xxx.find(" ") + 1); // Remove the Y coordinate
+							cout << "Found X: " << x << " Found Y: " << y << endl;
+							Coordinates coordinates(x, y); // CREATE THE COORDINATES BASED ON THE VALUES WE JUST GOT
+							route.addNode(coordinates); // ADD THE NODE TO THE ROUTE
+							// now I think we can integrate that route easly into the list
+							
+
+
+
+						}
+
+						
+					}
+
+					allRoutes.addRoute(route);
+					cout << "Route added" << endl;
+					cout << "at index: " << allRoutes.size << endl;
+				}
+			}
+			cout << selectedRoute << endl;
 		}
-
-		
-
-
-
-		
-
-
-
-
-
-
-
-
 };
--- include/Routes.h
+++ include/Routes.h
@@ -27,7 +27,7 @@ public:
 	{
 		if (head == nullptr) {
 			head = new Node<Route>(route);
-			size++;
+			size = 1;
 		}
 		else{
 			Node<Route>* temp = head;
@@ -94,104 +94,6 @@ public:
     }
 
 
-    void save() {
-
-        ofstream file("userdata/routes.txt");
-        if (!file.is_open()) {
-            cerr << "ERROR GARRAFAL: SE QUEMARON LOS FRIJOLES." << endl;
-            return;
-        }
-
-        Node<Route>* temp = head; // MAKE A TEMPORARY POINTER TO THE HEAD OF THE LIST
-        Node<Coordinates>* subTemp = nullptr; // MAKE A TEMPORARY POINTER TO THE HEAD OF THE SUB-LIST
-
-        while (temp != nullptr) { // ITERATE THROUGH THE ROUTES
-
-            file << "<Route>" << endl;
-            file << "Color: ";
-            file << static_cast<int>(temp->data.color.r) << " " // SAVE THE R CHANNEL INTO THE FILE
-                << static_cast<int>(temp->data.color.g) << " " // SAVE THE G CHANNEL INTO THE FILE
-                << static_cast<int>(temp->data.color.b) << std::endl; // SAVE THE B CHANNEL INTO THE FILE
-
-            subTemp = temp->data.head; // MAKE A TEMPORARY POINTER TO THE HEAD OF THE SUB-LIST
-
-            while (subTemp != nullptr) { // ITERATE THROUGH THE SUB-LIST
-                file << subTemp->data.getX() << " " << subTemp->data.getY() << endl; // SAVE THE COORDINATES INTO THE FILE
-                subTemp = subTemp->next; // MOVE TO THE NEXT NODE
-            }
-            file << "</>" << endl; // END OF THE ROUTE
-            temp = temp->next; // MOVE TO THE NEXT ROUTE
-        }
-
-    }
-
-    void load(Node<Route>* routeListPTR) {
-        ifstream file("userdata/routes.txt");
-        if (!file.is_open()) {
-            cerr << "ERROR GARRAFAL: SE QUEMARON LOS FRIJOLES." << endl;
-            return;
-        }
-
-        string xxx;
-        while (getline(file, xxx)) {
-            if (xxx == "<Route>") {
-                // Start a new route
-                Route route;
-                cout << "Antes del getline skipeo: " << xxx << endl;
-                getline(file, xxx); // Skip the "<Route>" line
-                cout << "Despues del getline skipeo: "<< xxx << endl;
-                // xxx should look like "Color: 255 255 255"
-                // We need to split the string into 3 substrings and convert them to integers, then assign them to the color and create a new route easy peasy
-                // xxx is line
-                 int r, g, b;
-                 xxx.erase(0, 7); // Remove the "Color: " part
-                 r = stoi(xxx.substr(0, xxx.find(" "))); // Get the R channel
-                 xxx.erase(0, xxx.find(" ") + 1); // Remove the R channel
-                 g = stoi(xxx.substr(0, xxx.find(" "))); // Get the G channel
-                 xxx.erase(0, xxx.find(" ") + 1); // Remove the G channel
-                 b = stoi(xxx.substr(0, xxx.find(" "))); // Get the B channel
-                 xxx.erase(0, xxx.find(" ") + 1); // Remove the B channel
-                 cout << "Found R: " << r << " Found G: " << g << " Found B: " << b << endl;
-
-                 Color color(r, g, b); // CREATE THE COLOR BASED ON THE VALUES WE JUST GOT
-                 route.color = color; // ASSIGN THE COLOR TO THE ROUTE
-
-                // Now we need to get the coordinates
-                    while (getline(file, xxx)) {
-					if (xxx == "</>") {
-						// End of the route
-						break;
-					}
-					else {
-						// Add the coordinates to the route
-						int x, y;
-						x = stoi(xxx.substr(0, xxx.find(" "))); // Get the X coordinate
-						xxx.erase(0, xxx.find(" ") + 1); // Remove the X coordinate
-						y = stoi(xxx.substr(0, xxx.find(" "))); // Get the Y coordinate
-						xxx.erase(0, xxx.find(" ") + 1); // Remove the Y coordinate
-						cout << "Found X: " << x << " Found Y: " << y << endl;
-						Coordinates coordinates(x, y); // CREATE THE COORDINATES BASED ON THE VALUES WE JUST GOT
-                        route.addNode(coordinates); // ADD THE NODE TO THE ROUTE
-						// the thing is, how do we add that route to the list?
-                        // now we have a pointer to the first route in the list
-                        // we need to iterate through the list until we find the last route
-                        // then we add the new route to the end of the list
-						Node<Route>* temp = routeListPTR;
-						while (temp->next != nullptr) {
-							temp = temp->next;
-						}
-						temp->next = new Node<Route>(route);
-						routeListPTR->data.size++;
-
-					}
-                    
-				}
-
-
-                
-                // Add the new route to the list
-            }
-        }
-    }
+    
 
 };
